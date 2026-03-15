ebe956ff676053ba9b8569199f159fbcc41f4b59
drzzln <drzzln@protonmail.com>
2026-03-15 19:56:41 -0400

    add nix flake with substrate integration

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  description = "Akeyless legacy Ruby SDK - auto-generated Ruby gem for the Akeyless Vault API";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs { inherit system; };
+    in {
+      devShells.default = pkgs.mkShellNoCC {
+        packages = with pkgs; [ ruby bundler ];
+      };
+    });
+}
